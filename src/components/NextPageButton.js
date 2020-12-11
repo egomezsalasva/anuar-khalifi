70d4cae6e642aea366b8329528df3e2243a7aaa0
Kike <kike@Kikes-MBP.home>
2020-12-11 16:17:48 +0100

    next page button mobile

--- src/components/NextPageButton.js
+++ src/components/NextPageButton.js
@@ -9,6 +9,9 @@ const ButtonContainer = styled.div`
     height: 180px;
     background: ${ props => props.theme.mode === 'dark' ? colors.white :  colors.blue };
     margin-top: 130px;
+    @media (max-width: ${mediaQueries.mobile}) {
+        height: 120px;
+    }
 
     .link{
         width: 100%;
@@ -23,11 +26,16 @@ const ButtonContainer = styled.div`
         font-size: 34px;
         color: ${ props => props.theme.mode === 'dark' ? colors.black :  colors.white };
         text-align: center;
+        @media (max-width: ${mediaQueries.mobile}) {
+            font-size: 21px;
+        }
 
         span{
             font-size: 20px;
             margin-right: 10px;
             @media (max-width: ${mediaQueries.mobile}) {
+                font-size: 13px;
+                margin-right: 0px;
                 display: block;
             }
         }
